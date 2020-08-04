8b36cb2094c33e97748ad928959b70baebdfea2a
Ben Wilcock <benwilcock@gmail.com>
2020-08-04 11:09:45 +0100

    used the a code formatter and updated the ignores

--- web.py
+++ web.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from markupsafe import escape
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-@app.route('/hello/<username>')
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+@app.route("/hello/<username>")
 def hello_user(username):
     # say hello to that user
-    return 'Hello %s' % escape(username)
+    return "Hello %s" % escape(username)
