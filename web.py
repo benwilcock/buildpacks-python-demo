7b001fe3d32a8916f3e08ec9ca80aa81838dc56f
Ben Wilcock <benwilcock@gmail.com>
2020-08-06 15:26:22 +0100

    added info level logging

--- web.py
+++ web.py
@@ -1,18 +1,18 @@
+import logging
 from flask import Flask
 from markupsafe import escape
 
 app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
 
 @app.route("/")
 def hello_world():
+    app.logger.info('default path')
     return "Hello, World!"
 
 
 @app.route("/hello/<username>")
 def hello_user(username):
     # say hello to that user
+    app.logger.info('/hello path. User: ' + username)
     return "Hello %s" % escape(username)
-
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
