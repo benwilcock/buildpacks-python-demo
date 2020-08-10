5ed1139a932ed86adca6b505db0ae7af68e5f3e4
Ben Wilcock <benwilcock@gmail.com>
2020-08-10 15:31:25 +0100

    ran the code formatter

--- web.py
+++ web.py
@@ -5,14 +5,15 @@ from markupsafe import escape
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
 
+
 @app.route("/")
 def hello_world():
-    app.logger.info('default path')
+    app.logger.info("default path")
     return "Hello, World!"
 
 
 @app.route("/hello/<username>")
 def hello_user(username):
     # say hello to that user
-    app.logger.info('/hello path. User: ' + username)
+    app.logger.info("/hello path. User: " + username)
     return "Hello %s" % escape(username)
